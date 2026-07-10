22ac653318bf67582a1f86aa906a603173c0919e
PRASHIK-PharmTech <meshramprashik407@gmail.com>
2026-07-11 05:15:39 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("PharmaSathi AI Loaded Successfully");
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        alert(card.innerText + " feature is under development.");
+    });
+});
